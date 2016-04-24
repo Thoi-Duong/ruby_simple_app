56d361a9bf2cc5979f06de076d928a19d6cfc832
Thoi Duong <dnhthoi@gmail.com>
2016-04-24 19:12:38 +0000

    add account activation

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,20 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
-             password:              "654321",
-             password_confirmation: "654321",
-             admin: true)
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
-  password = "123456"
+  password = "password"
   User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
+              email: email,
+              password:              password,
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
 end
